4adbb8bb73607e07ed8da8eb79f0dcf4fa8bf548
shubham negi <negishubham93@gmail.com>
2024-08-10 21:11:31 +0530

    refactored authorize middleware

--- src/middleware/authorize.middleware.ts
+++ src/middleware/authorize.middleware.ts
@@ -5,13 +5,9 @@ import User from "../user/user.model";
 export const authorize = (...roles: USER_ROLE_TYPE[]) => {
     return async (req: any, res: any, next: any) => {
       try {
-        const user = await User.findById(req.user._id);
-        if (!user) {
-          return res.status(401).send({ error: 'User not found.' });
-        }
+        let role = req.user.role
   
-        // Assuming roles is an array of strings and user.roles[0] is the role of the user
-        if (!roles.includes(user.role as USER_ROLE_TYPE)) {
+        if (!roles.includes(role as USER_ROLE_TYPE)) {
           return res.status(403).send({ error: 'Access denied.' });
         }
   
